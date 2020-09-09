2ab9716c55d9c9160238823cbe0b64a4990b3896
casjay <casjay@yahoo.com>
2020-09-09 15:58:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-mkdir -p /srv/docker/airsonic/{music,podcasts,playlists,data} && chmod -Rf 777 /srv/docker/airsonic
+APPNAME="airsonic"
+DATADIR="/srv/docker/$APPNAME"
+
+mkdir -p "$DATADIR"/{music,podcasts,playlists,data} && chmod -Rf 777 "$DATADIR"
+
+if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
+docker pull airsonic/airsonic:latest && docker restart $APPNAME
+else
 
 docker run -d \
---name=airsonic \
---hostname airsonic \
+--name="$APPNAME" \
+--hostname "$APPNAME" \
 --restart=always \
 --privileged \
 -p 4040:4040 \
--v /srv/docker/airsonic/data:/airsonic/data:z \
--v /srv/docker/airsonic/music:/airsonic/music:z \
--v /srv/docker/airsonic/podcasts:/airsonic/podcasts:z \
--v /srv/docker/airsonic/playlists:/airsonic/playlists:z \
+-v $DATADIR/data:/airsonic/data:z \
+-v $DATADIR/music:/airsonic/music:z \
+-v $DATADIR/podcasts:/airsonic/podcasts:z \
+-v $DATADIR/playlists:/airsonic/playlists:z \
 airsonic/airsonic:latest
