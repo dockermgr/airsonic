ef7569968e818a03cbdb1fa6b5c83b187851a72f
Jason <jason@casjay.net>
2021-01-29 23:27:51 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
 APPNAME="airsonic"
-DATADIR="/srv/docker/$APPNAME"
 
 mkdir -p "$DATADIR"/{music,podcasts,playlists,data} && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker pull airsonic/airsonic:latest && docker restart $APPNAME
+  docker pull airsonic/airsonic:latest && docker restart "$APPNAME"
 else
-
-docker run -d \
---name="$APPNAME" \
---hostname "$APPNAME" \
---restart=always \
---privileged \
--p 4040:4040 \
--v $DATADIR/data:/airsonic/data:z \
--v $DATADIR/music:/airsonic/music:z \
--v $DATADIR/podcasts:/airsonic/podcasts:z \
--v $DATADIR/playlists:/airsonic/playlists:z \
-airsonic/airsonic:latest
+  docker run -d \
+    --name="$APPNAME" \
+    --hostname "$APPNAME" \
+    --restart=always \
+    --privileged \
+    -p 4040:4040 \
+    -v "$DATADIR/data":/airsonic/data:z \
+    -v "$DATADIR/music":/airsonic/music:z \
+    -v "$DATADIR/podcasts":/airsonic/podcasts:z \
+    -v "$DATADIR/playlists":/airsonic/playlists:z \
+    airsonic/airsonic:latest
+fi
