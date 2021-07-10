8f362bfefd611b2ad5f3686dad7f07fca58b6851
Jason <jason@casjay.net>
2021-07-10 15:53:58 -0400

    🐛 FIXED: install.sh 🐛

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 APPNAME="airsonic"
+DOCKER_HUB_URL="airsonic/airsonic:latest"
 
-sudo mkdir -p "$DATADIR"/{music,podcasts,playlists,data} && sudo chmod -Rf 777 "$DATADIR"
+sudo mkdir -p "$DATADIR"/{music,podcasts,playlists,data}
+sudo chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-  sudo docker pull airsonic/airsonic:latest && docker restart "$APPNAME"
+  sudo docker pull "$DOCKER_HUB_URL"
+  sudo docker restart "$APPNAME"
 else
   sudo docker run -d \
     --name="$APPNAME" \
@@ -16,6 +19,6 @@ else
     -v "$DATADIR/data":/airsonic/data:z \
     -v "$DATADIR/music":/airsonic/music:z \
     -v "$DATADIR/podcasts":/airsonic/podcasts:z \
-    -v "$DATADIR/playlists":/airsonic/playlists:z \
-    airsonic/airsonic:latest
+    -v "$DATADIR/playlists":/airsonic/playlists:z
+  "$DOCKER_HUB_URL"
 fi
