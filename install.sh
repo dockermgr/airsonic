2edb382c0bd24c7b7ec203d1d836e5f6bea8ee85
Jason <jason@casjay.net>
2021-07-31 12:09:04 -0400

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,11 +1,53 @@
 #!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME="$(basename "$(dirname $PWD/install.sh)")"
+VERSION="202107311147-git"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
+SRC_DIR="${BASH_SOURCE%/*}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#set opts
 
-APPNAME="airsonic"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202107311147-git
+# @Author        : casjay
+# @Contact       : casjay
+# @License       : WTFPL
+# @ReadME        : dockermgr --help
+# @Copyright     : Copyright: (c) 2021 casjay, casjay
+# @Created       : Saturday, Jul 31, 2021 11:47 EDT
+# @File          : airsonic
+# @Description   : airsonic container installer
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Import functions
+CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}/functions"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-testing.bash}"
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/$GIT_DEFAULT_BRANCH/functions}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ -f "$PWD/$SCRIPTSFUNCTFILE" ]; then
+  . "$PWD/$SCRIPTSFUNCTFILE"
+elif [ -f "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE"
+else
+  echo "Can not load the functions file: $SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" 1>&2
+  exit 1
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# user system devenv dfmgr dockermgr fontmgr iconmgr pkmgr systemmgr thememgr wallpapermgr
+system_install
+__options "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Begin installer
+DATADIR="${DOCKERMGR_HOME:-/srv/docker/$APPNAME}"
 DOCKER_HUB_URL="airsonic/airsonic:latest"
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 sudo mkdir -p "$DATADIR"/{music,podcasts,playlists,data}
 sudo chmod -Rf 777 "$DATADIR"
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
   sudo docker pull "$DOCKER_HUB_URL"
   sudo docker restart "$APPNAME"
@@ -22,3 +64,6 @@ else
     -v "$DATADIR/playlists":/airsonic/playlists:z
   "$DOCKER_HUB_URL"
 fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End script
+exit $?
